5bdc366c76f09826784982086ab7666753075776
Rohit Pandey <56378143+rohit-pandey96@users.noreply.github.com>
2020-05-06 02:25:33 +0530

    4th commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiurl : 'https://api.covid19india.org/state_district_wise.json',
+  graphDataUrl : 'https://api.covid19india.org/data.json'
 };
